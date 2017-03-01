2d218f5c8df09e59161575fd8611a93cd810a28a
boostNode <support@boostnode.com>
2017-03-01 10:11:26 -0600

    original

---
+++ CarDealer/CarDealer/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+ *  Author: Troy Davis
+ *  Project: Module06 - CarDealer - Console
+ *  Class: IN 2017 (Advanced C#)
+ *  Date: Feb 23, 2017 
+ *  Revision: Original
+ */
+
+using System;
+
+namespace CarDealer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // greeting
+            Console.WriteLine("Module 06 - Car Dealer");
+
+            // Car example 1
+            Car myCar = new Car("Ford", "Mustang", 1967, "Pebble Beige");
+            myCar.DisplayProperties("My old car");
+
+            // Car example 2
+            Car myNewCar = new Car();
+            myNewCar.Make = "Nissan";
+            myNewCar.Model = "Titan";
+            myNewCar.Year = 2007;
+            myNewCar.Color = "Red";
+            myNewCar.DisplayProperties("My new car");
+
+            // wait on user to close console
+            Console.Write("\nPress 'Enter' to exit: "); Console.ReadLine();
+        }
+    }
+    class Car
+    {
+        // constructors
+        public Car()
+        {
+            // default constructor
+        }
+        public Car(string make, string model, int year, string color)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.Color = color;
+        }
+
+        // setters / getters
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        
+        // methods
+        public void DisplayProperties(string description)
+        {
+            Console.WriteLine("\n\t{0} is a {1} {2} {3}. It's color is {4}.", description, this.Year, this.Make, this.Model, this.Color);
+        }
+    }
+}
---
+++ Lesson02/Lesson02/Program.cs
@@ -0,0 +1,94 @@
+﻿/*
+ *  Author: Troy Davis
+ *  Project: Module06 - Lesson02 - Console
+ *  Class: IN 2017 (Advanced C#)
+ *  Date: Feb 23, 2017 
+ *  Revision: Original
+ */
+
+using System;
+
+namespace Lesson02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // greeting
+            Console.WriteLine("Module 06 - Lesson 02\n");
+
+            // example instantiation 1
+            Rectangle rect1 = new Rectangle(10.0, 20.0);// using overloaded constructor
+            double area1 = rect1.GetArea();
+            Console.WriteLine("\tArea of Rectangle 1: {0}", area1);
+
+            // example instantiation 2
+            Rectangle rect2 = new Rectangle();// using default constructor
+            rect2.Length = 15.0;
+            rect2.Width = 22.0;
+            double area2 = rect2.GetArea();
+            Console.WriteLine("\tArea of Rectangle 2: {0}", area2);
+
+            // wait on user to close console
+            Console.Write("\nPress 'Enter' to exit: "); Console.ReadLine();
+        }
+        class Rectangle
+        {
+            // contructors
+            public Rectangle()
+            {
+                // default constructor - doing nothing here
+            }
+            public Rectangle(double length, double width)
+            {
+                // example of using this keyword; allows input field names to shadow class property names
+                this.length = length;
+                this.width = width;
+            }
+
+            // properties (manually implemented)
+            private double length;
+            private double width;
+            public double Length
+            {
+                get
+                {
+                    return length;
+                }
+                set
+                {
+                    if (value > 0.0)
+                        length = value;
+                }
+            }
+            public double Width
+            {
+                get
+                {
+                    return width;
+                }
+                set
+                {
+                    if (value > 0.0)
+                        width = value;
+                }
+            }
+            // properties (auto-implemented)
+            /*
+            public double Length { get; set; }
+            public double Width { get; set; }
+            */
+
+            // methods
+            public void InitFields(double l, double w)
+            {
+                length = l;
+                width = w;
+            }
+            public double GetArea()
+            {
+                return length * width;
+            }
+        }
+    }
+}
